9515ae86d20591e9af1fbbc6365c0337207ae4a1
Joern <924901972@qq.com>
2023-03-27 21:59:00 +0800

    feat: add proxy example

--- 3-proxy.js
+++ 3-proxy.js
@@ -87,3 +87,113 @@ var proxyImage = (function(){
 })()
 
 proxyImage.setSrc('http://xxx/x.png');
+
+/**
+ * 代理对象合并http请求
+ */
+
+var synchronousFile = function(id){
+    console.log('开始同步文件,id为:' + id);
+};
+
+var proxySynchronousFile = (function(){
+    var cache = [], timer;
+    return function(id){
+        // 保证不会覆盖已经启动的定时器
+        if(timer){
+            return;
+        }
+
+        timer = setTimeout(function(){
+            synchronousFile(id);
+            clearTimeout(timer);
+            timer = null;
+            cache.length = 0;
+        },2000)
+    }
+})()
+
+var checkbox = document.getElementsByTagName('input');
+for(var i = 0, c; c = checkbox[i++];){
+    c.onclick = function(){
+        if(this.checked === true){
+            proxySynchronousFile(this.id)
+        }
+    }
+}
+
+/**
+ * 惰性加载console.js工具类
+ */
+var cache = [];
+
+// 未加载真正console.js之前的代码
+var miniConsole = {
+    log: function(){
+        var args = arguments;
+        cache.push(function(){
+            return miniConsole.log.apply(miniConsole, args);
+        })
+    }
+}
+
+miniConsole.log(1);
+
+// 用户按下F2，开始加载真正的miniConsole.js
+document.body.addEventListener('keydown', (ev) => {
+    if(ev.code === 113){
+        var script = document.createElement('script');
+        script.onload = function(){
+            for(var i = 0, fn; fn = cache[i++];){
+                fn();
+            }
+        }
+        script.src = 'miniConsole.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+}, false);
+
+// 真正的miniConsole.js代码
+miniConsole = {
+    log: function(){
+        // 真正的代码略
+        console.log(Array.prototype.join.call(arguments));
+    }
+}
+
+/**
+ * 缓存代理(脑补耗时计算过程)
+ */
+
+// 乘积算法
+var mult = function(){
+    var a = 1;
+    for(var i = 0, l = arguments.length; i < l; i++ ){
+        a = a*arguments[i];
+    }
+    return a;
+}
+
+// 累加算法
+var plus = function(){
+    var a = 1;
+    for(var i = 0, l = arguments.length; i < l; i++ ){
+        a = a+arguments[i];
+    }
+    return a;
+}
+
+// 缓存代理包裹
+var createProxyFactory = function(fn){
+    var cache = {};
+    return function(){
+        var args = Array.prototype.join.call(arguments, ',');
+        if(args in cache){
+            return cache[args];
+        }
+        return cache[args] = fn.apply(this.arguments);
+    }
+}
+
+var proxyMult = createProxyFactory(mult);
+var proxyPlus = createProxyFactory(plus);
