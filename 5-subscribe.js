1759774cc0f9df5426a9303e061960199087fcb0
Joern <924901972@qq.com>
2023-04-17 21:32:22 +0800

    feat: add subscribe example

--- 5-subscribe.js
+++ 5-subscribe.js
@@ -32,7 +32,7 @@ let commonEvent = {
 
         if(!fn){
             // 没有传入取消订阅的函数，默认取消所有
-            fns && fns.length = 0;
+            fns && (fns.length = 0);
         } else {
             for(let l = fns.length - 1; l >= 0; l--){
                 const _fn = fns[l];
@@ -104,3 +104,83 @@ const nav = (function(){
         }
     }
 })();
+
+/**
+ * 全局Event
+ */
+
+let Event = (function(){
+    var clientList = {},listen,trigger,remove;
+
+    listen = (key, fn) => {
+        if(!this.clientList[key]){
+            this.clientList[key] = [];
+        }
+        this.clientList[key].push(fn); // 订阅的消息添加进缓存列表
+    };
+
+    // 触发广播
+    trggier = () => {
+        let key = Array.prototype.shift.call(arguments);
+        let fns = this.clientList[key];
+        if(!fns || fns.length === 0){
+            return false;
+        }
+        for(let i = 0; i < fns.length; i++){
+            const fn = fns[i];
+            fn.apply(this, arguments);
+        }
+    };
+    // 取消订阅
+    remove = (key, fn) => {
+        const fns = this.clientList[key];
+
+        if(!fns){
+            return false;
+        }
+
+        if(!fn){
+            // 没有传入取消订阅的函数，默认取消所有
+            fns && (fns.length = 0);
+        } else {
+            for(let l = fns.length - 1; l >= 0; l--){
+                const _fn = fns[l];
+                if(_fn === fn){
+                    // 删除此回调函数
+                    fns.splice(l, 1);
+                }
+            }
+        }
+    };
+
+    return {
+        listen,
+        trigger,
+        remove
+    }
+})();
+
+Event.listen('squareMeter88', function(price)){
+    // 小红订阅消息
+    console.log('价格=' + price);
+}
+
+Event.trigger('squareMeter88', 200000); // 售楼处发布消息
+
+/**
+ * 模块通信
+ */
+var a = (function(){
+    var count = 0;
+    var button = document.getElementById('count');
+    button.onclick = function(){
+        Event.trggier('add',count++);
+    }
+})()
+
+var b = (function(){
+    var div = document.getElementById('show');
+    Event.listen('add',function(count){
+        div.innerHTML = count;
+    });
+})()
