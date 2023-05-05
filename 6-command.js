bd0b8670ad5c4063a574320c21775625f5e3e6ac
Joern <924901972@qq.com>
2023-05-05 22:25:26 +0800

    feat: add command example

--- 6-command.js
+++ 6-command.js
@@ -61,3 +61,99 @@ var deleteSubMenuBarCommnad = new DeleteSubMenuBarCommnad(SubMenu);
 setCommand(button1, refreshMenuBarCommnad);
 setCommand(button2, addSubMenuBarCommnad);
 setCommand(button3, deleteSubMenuBarCommnad);
+
+/**
+ * 简单的实现
+ */
+
+var bindClick = function(button,func){
+    button.onclick = func;
+}
+
+var MenuBar = {
+    refresh: function(){
+        console.log('刷新')
+    }
+}
+
+var SubMenu = {
+    add: function(){},
+    delete: function(){}
+}
+
+bindClick(button1, MenuBar.refresh);
+bindClick(button2, SubMenu.add);
+
+/**
+ * 基于闭包的实现
+ */
+var setCommand = function(button,command){
+    button.onclick = function(){
+        command.execute();
+    }
+}
+
+var MenuBar = {
+    refresh: function(){
+        console.log('刷新')
+    }
+}
+
+var RefreshMenuBarCommnad = function(receiver){
+    return {
+        execute: function(){
+            receiver.refresh();
+        }
+    }
+}
+
+var refreshMenuBarCommnad = RefreshMenuBarCommnad(MenuBar);
+setCommand(button1, refreshMenuBarCommnad);
+
+/**
+ * 小球移动Demo
+ */
+
+var ball = document.getElementById('ball');
+var pos = document.getElementById('pos');
+var moveBtn = document.getElementById('moveBtn');
+
+moveBtn.onclick = function(){
+    var animate = new Animate(ball);
+    animate.start('left',pos.value, 1000, 'strongEaseOut');
+}
+
+/**
+ * 命令模式+撤销
+ */
+ var ball = document.getElementById('ball');
+ var pos = document.getElementById('pos');
+ var moveBtn = document.getElementById('moveBtn');
+ var cancelBtn = document.getElementById('cancelBtn');
+
+ var MoveCommand = function(receiver, pos){
+    this.receiver = receiver;
+    this.pos = pos;
+    this.oldPos = null;
+ }
+
+ MoveCommand.prototype.execute = function(){
+    this.receiver.start('left',this.pos, 1000, 'strongEaseOut');
+    this.oldPos = this.receiver.dom.getBoundingClientRect()[this.receiver.propertyName];
+ }
+
+ MoveCommand.prototype.undo = function(){
+    this.receiver.start('left',this.oldPos, 1000, 'strongEaseOut');
+ }
+
+ var moveCommand;
+
+ moveBtn.onclick = function(){
+    var animate = new Animate(ball);
+    moveCommand = new MoveCommand(animate, pos.value);
+    moveCommand.execute();
+ }
+
+ cancelBtn.onclick = function(){
+    moveCommand.undo();
+ }
