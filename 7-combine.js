6ab8a1aadab68e4c368bb852535b9de2ab69ce88
Joern <924901972@qq.com>
2023-05-16 21:50:31 +0800

    feat: add combine example

--- 7-combine.js
+++ 7-combine.js
@@ -72,3 +72,51 @@ var setCommand = (function(command){
         command.execute();
     }
 })(macroCommand)
+
+/**
+ * 组合模式实现扫描文件夹
+ */
+// 定义Folder与File类
+class Folder {
+    constructor(name) {
+        this.name = name;
+        this.files = [];
+    }
+    add(file) {
+        this.files.push(file);
+    }
+    scan() {
+        for (let i = 0, file, files = this.files; files = files[i++];) {
+            file.scan();
+        }
+    }
+}
+
+class File {
+    constructor(name) {
+        this.name = name;
+    }
+    add(file) {
+        throw new Error('文件下面不能添加文件')
+    }
+    scan() {
+        for (let i = 0, file, files = this.files; files = files[i++];) {
+            file.scan();
+        }
+    }
+}
+
+// 创建实例，组合
+const folder = new Folder('学习资料');
+const folder1 = new Folder('JS');
+
+const file1 = new File('JS设计模式');
+const file2 = new File('精通Jquery');
+
+// 用户操作对象，无需分辨是文件还是文件夹，很容易添加到整个组合树中 --- 符合开闭原则
+folder1.add(file1);
+folder1.add(file2);
+folder.add(folder1);
+
+// 扫描操作也很简单
+folder.scan()
