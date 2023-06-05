26e68cb085e9096a611aa17c1a688524ac67230a
Joern <924901972@qq.com>
2023-06-05 21:03:09 +0800

    feat: add flyweight example

--- 9-flyweight.js
+++ 9-flyweight.js
@@ -84,3 +84,71 @@ window.startUpload = (uploadType, files){
         uploadObj.init(id++);
     }
 }
+
+/**
+ * 享元模式重构
+ */
+ class Upload {
+    constructor(uploadType){
+        // 只保留内部状态
+        this.uploadType = uploadType;
+    }
+
+    //原先init不再需要，初始化转移到uploadManager的add函数中
+
+    delFile = (id) => {
+        // 把id对应对象的外部状态组装到共享对象中，因为下面需要获取fileSize了
+        uploadManager.setExternalState(id, this);
+
+        if(this.fileSize < 3000){
+            return this.dom.parentNode.removeChild(this.dom)
+        }
+
+        if(window.confirm('确定要删除该文件吗？')){
+            return this.dom.parentNode.removeChild(this.dom)
+        }
+    }
+}
+
+// 工厂负责创建Upload对象，判断是否需要返回共享对象
+const UploadFactory = {
+    createdFlyWeightObjs: {},
+    create: function(uploadType){
+        if(this.createdFlyWeightObjs[uploadType]){
+            return this.createdFlyWeightObjs[uploadType]
+        }
+        return this.createdFlyWeightObjs[uploadType] = new Upload(uploadType)
+    }
+}
+
+const uploadManager = {
+    uplodaDatabase: {},
+    add: function(id, uploadType, fileName, fileSize){
+        const flyweightObj = UploadFactory.create(uploadType);
+        const dom = document.createElement('div');
+        dom.innerHTML = `<span>文件名称${this.fileName}，文件大小${this.fileSize}</span><button class="delFile">删除</button>`
+        dom.querySelector('.delFile').onclikc = () => {
+            flyweightObj.delFile(id);
+        }
+        document.body.appendChild(this.dom);
+        this.uplodaDatabase[id] = {
+            fileName,
+            fileSize,
+            dom
+        };
+        return flyweightObj;
+    },
+    setExternalState: function(id, flyweightObj){
+        const uploadData = this.uplodaDatabase[id];
+        for(let i in uploadData){
+            flyweightObj[i] = uploadData[i];
+        }
+    }
+}
+
+let id2 = 0;
+window.startUpload = function(upploadType, files){
+    for(let i=0,file;file = files[i++];){
+        uploadManager.add(++id2, upploadType, file.fileName, file.fileSize);
+    }
+}
