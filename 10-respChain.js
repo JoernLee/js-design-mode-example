13360bea26534dfb8a5256e8fdf5652833778a2c
Joern <924901972@qq.com>
2023-07-07 20:54:11 +0800

    feat: add respChain example

--- 10-respChain.js
+++ 10-respChain.js
@@ -21,7 +21,7 @@ let order = (orderType, pay, stock) => {
 /**
  * 第一次重构
  */
-let order500 = (orderType, pay, stock) => {
+var order500 = (orderType, pay, stock) => {
     if(orderType === 1 && pay){
         console.log('500元定金预购，得到100优惠券')
     } else {
@@ -30,7 +30,7 @@ let order500 = (orderType, pay, stock) => {
     }
 }
 
-let order200 = (orderType, pay, stock) => {
+var order200 = (orderType, pay, stock) => {
     if(orderType === 2 && pay){
         console.log('200元定金预购，得到50优惠券')
     } else {
@@ -39,7 +39,7 @@ let order200 = (orderType, pay, stock) => {
     }
 }
 
-let orderNormal = (orderType, pay, stock) => {
+var orderNormal = (orderType, pay, stock) => {
     if(stock > 0){
         console.log('普通购买')
     } else {
@@ -48,4 +48,64 @@ let orderNormal = (orderType, pay, stock) => {
 }
 
 order500(1,false,500);
+// ...
+
+/**
+ * 灵活可拆分
+ */
+var order500 = (orderType, pay, stock) => {
+    if(orderType === 1 && pay){
+        console.log('500元定金预购，得到100优惠券')
+    } else {
+        return 'nextSuccessor'
+    }
+}
+
+var order200 = (orderType, pay, stock) => {
+    if(orderType === 2 && pay){
+        console.log('200元定金预购，得到50优惠券')
+    } else {
+        return 'nextSuccessor'
+    }
+}
+
+var orderNormal = (orderType, pay, stock) => {
+    if(stock > 0){
+        console.log('普通购买')
+    } else {
+        console.log('库存不足')
+    }
+}
+
+class Chain {
+    constructor(fn){
+        this.fn = fn;
+        this.successor = null;
+    }
+
+    // 指定链中下一个节点
+    setNextSuccessor = function(successor){
+        return this.successor = successor;
+    }
+
+    // 传递请求
+    pastRequest = function(){
+        const ret = this.fn.apply(this, arguments);
+        if(ret = 'nextSuccessor'){
+            return this.successor && this.successor.pastRequest.apply(this.successor, arguments);
+        }
+        return ret;
+    }
+}
+
+let chainOrder500 = new Chain(order500);
+let chainOrder200 = new Chain(order200);
+let chainOrderNormal = new Chain(orderNormal);
+
+// 指定顺序
+chainOrder500.setNextSuccessor(chainOrder200);
+chainOrder200.setNextSuccessor(chainOrderNormal);
+
+// 只需要关注第一个节点
+chainOrder500.pastRequest(1,true, 500);
 // ...
