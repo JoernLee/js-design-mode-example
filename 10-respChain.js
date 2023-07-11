e405f6b6567a0781f4eb29af00c55d4c80621520
Joern <924901972@qq.com>
2023-07-11 21:24:18 +0800

    feat: add respChain example

--- 10-respChain.js
+++ 10-respChain.js
@@ -109,3 +109,86 @@ chainOrder200.setNextSuccessor(chainOrderNormal);
 // 只需要关注第一个节点
 chainOrder500.pastRequest(1,true, 500);
 // ...
+
+/**
+ * 异步职责链
+ */
+ class Chain {
+    constructor(fn){
+        this.fn = fn;
+        this.successor = null;
+    }
+
+    // ...
+
+    // 新增一个方法
+    next = function(){
+        return this.successor && this.successor.pastRequest.apply(this.successor, arguments);
+    }
+}
+
+let fn1 = new Chain(() => {
+    console.log(1);
+    return 'nextSuccessor';
+})
+
+let asyncFn2 = new Chain(() => {
+    console.log(2);
+    let self = this;
+    setTimeout(() => {
+        self.next();
+    }, 1000);
+})
+
+
+let fn3 = new Chain(() => {
+    console.log(3);
+})
+
+fn1.setNextSuccessor(asyncFn2).setNextSuccessor(fn3);
+fn1.pastRequest();
+
+/**
+ * 利用AOP来实现
+ */
+Function.prototype.after = function(fn){
+    let self = this;
+    return function(){
+        var ret = self.apply(this, arguments);
+        if(ret === 'nextSuccessor'){
+            return fn.apply(this,arguments);
+        }
+        return ret;
+    }
+}
+
+const orderChain = order500.after(order200).after(orderNormal);
+orderChain(1, true, 500);
+
+
+/**
+ * 职责链重构获取文件上传对象
+ */
+const getActiveUploadObj = () => {
+    try{
+        return new ActiveXObject("TXFTNActiveX.FTNUpload");
+    }catch(e){
+        return 'nextSuccessor';
+    }
+}
+
+const getFlashUploadObj = () => {
+    if(supoortFlash()){
+        const str = '<object type="application/x-shockwave-flash"></object>';
+        return $(str).appendTo($('body'));
+    }
+    return 'nextSuccessor';
+}
+
+const getFormUploadObj = () => {
+    return $('<form><input ....>...').appendTo($('body'))
+}
+
+const getUploadObj = getActiveUploadObj.after(getFlashUploadObj).after(getFormUploadObj);
+
+console.log(getUploadObj());
