884a4d928257ca04e78678d7fa9f600c89356a8b
Joern <924901972@qq.com>
2023-07-26 22:01:02 +0800

    feat: add decorator example

--- 12-decorator.js
+++ 12-decorator.js
@@ -16,3 +16,31 @@ MissileDecorator.prototype.fire = function(){
 let plane = new Plane();
 plane = new MissileDecorator(plane);
 plane.fire(); // 输出 发射普通子弹 发射导弹
+
+/**
+ * 保存引用来新增功能
+ */
+let a = function(){
+    console.log(1)
+}
+
+let _a = a;
+
+a = function(){
+    _a();
+    console.log(2);
+}
+
+a();
+
+// 实际开发中修改全局函数也会这样 - 避免覆盖之前函数的行为
+window.onload = function(){
+    console.log(1)
+}
+
+let _onload = window.onload || function(){};
+
+window.onload = function(){
+    _onload();
+    console.log(2);
+}
